43b16578d951cd047ad91df408fe7247ee829922
Akmal Narziev <146104679+Akmaljon2008@users.noreply.github.com>
2024-03-05 17:53:49 +0300

    fc

---
+++ MainApp/Program.cs
@@ -0,0 +1,10 @@
+﻿using Domain.Models;
+using Infrastracture.Services;
+
+var cs = new CustomerService();
+var tr = new TranzactionService();
+var t = new Tranzaction();
+t.SenderId = 3;
+t.TakerId = 4;
+t.Sum = 150;
+tr.MakeTranzaction(t);
